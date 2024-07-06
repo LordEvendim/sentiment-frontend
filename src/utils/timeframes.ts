8d61336c18dd1fa70edb80dcb916c8d3e9435bce
LordEvendim <24799163+LordEvendim@users.noreply.github.com>
2024-07-06 16:03:25 +0200

    feat: timeframes

--- src/utils/timeframes.ts
+++ src/utils/timeframes.ts
@@ -1,20 +1,25 @@
-import { startOfDay, subDays, subMonths, subWeeks, subYears } from "date-fns";
+import { addDays, startOfDay, subDays, subMonths, subYears } from "date-fns";
 
 export type DashboardTimeframe =
-  | "last-week"
-  | "last-month"
+  | "last-7-days"
+  | "last-14-days"
+  | "last-30-days"
   | "last-90-days"
+  | "last-6-months"
   | "last-year";
 
 const timeframeStartFunctions: Record<DashboardTimeframe, (end: Date) => Date> =
   {
+    "last-7-days": (end) => subDays(end, 7),
+    "last-14-days": (end) => subDays(end, 14),
+    "last-30-days": (end) => subDays(end, 30),
     "last-90-days": (end) => subDays(end, 90),
-    "last-month": (end) => subMonths(end, 1),
-    "last-week": (end) => subWeeks(end, 1),
+    "last-6-months": (end) => subMonths(end, 3),
     "last-year": (end) => subYears(end, 1),
   };
 
 export const calculateTimeframeStart = (
   now: Date,
   timeframe: DashboardTimeframe
-) => startOfDay(timeframeStartFunctions[timeframe](subDays(now, 1)));
+) =>
+  startOfDay(addDays(timeframeStartFunctions[timeframe](subDays(now, 1)), 1));
